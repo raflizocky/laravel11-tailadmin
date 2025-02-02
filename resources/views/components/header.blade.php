169af7dfa1055355967e35a9090dbcc11ef6b8e7
rafli zocky <raflizocky@gmail.com>
2025-02-02 15:54:24 +0700

    create: header - component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+        eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template
+    </title>
+    <link rel="icon" href="{{ asset('tailadmin/build/favicon.ico') }}">
+    <link href="{{ asset('tailadmin/build/style.css') }}" rel="stylesheet">
+</head>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>
-        eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template
-    </title>
-    <link rel="icon" href="{{ asset('tailadmin/build/favicon.ico') }}">
-    <link href="{{ asset('tailadmin/build/style.css') }}" rel="stylesheet">
-</head>
+<x-header></x-header>
 
 <body x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
 $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" :class="{ 'dark text-bodydark bg-boxdark-2': darkMode === true }">
